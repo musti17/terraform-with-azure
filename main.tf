8c439d5b1ae8753c620de277ac0b16181ed54b72
Mustafa Zahid <mustafazahid167@gmail.com>
2023-12-22 23:59:19 +0500

    Virtual network and a VM provisioned on Azure cloud.

--- main.tf
+++ main.tf
@@ -18,3 +18,59 @@ resource "azurerm_resource_group" "terraformResource" {
   name     = "terraformLab1Resource"
   location = "West US"
 }
+
+resource "azurerm_virtual_network" "tfvnet" {
+  name                = "tfvnet"
+  resource_group_name = azurerm_resource_group.terraformResource.name
+  location            = azurerm_resource_group.terraformResource.location
+  address_space       = ["10.0.0.0/16"]
+  dns_servers         = ["10.0.0.4"]
+
+}
+
+resource "azurerm_subnet" "internal" {
+  name                 = "internal"
+  resource_group_name  = azurerm_resource_group.terraformResource.name
+  virtual_network_name = azurerm_virtual_network.tfvnet.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+resource "azurerm_network_interface" "tfnint" {
+  name                = "${var.prefix}-nic"
+  location            = azurerm_resource_group.terraformResource.location
+  resource_group_name = azurerm_resource_group.terraformResource.name
+
+  ip_configuration {
+    name                          = "testconfiguration1"
+    subnet_id                     = azurerm_subnet.internal.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_virtual_machine" "main" {
+  name                  = "${var.prefix}-vm"
+  location              = azurerm_resource_group.terraformResource.location
+  resource_group_name   = azurerm_resource_group.terraformResource.name
+  network_interface_ids = [azurerm_network_interface.tfnint.id]
+  vm_size               = "Standard_DS1_v2"
+
+
+  storage_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
+  storage_os_disk {
+    name              = "myosdisk1"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
+  tags = {
+    environment = "staging"
+  }
+}
